98037bbe76f86dc83165e7a2b3c3c86d5a32b5f1
aokorodu <aokorodu@gmail.com>
2023-11-05 16:07:49 -0500

    current condition data

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,47 @@ import WeatherDisplay from './components/weather-display';
 
 const url: string = "http://api.weatherapi.com/v1/current.json?key=7b006266b8fa412baec213059230411&q=19128&aqi=no";
 
+type Condition = {
+  code: number,
+  icon: string,
+  text: string
+}
+
+type CurrentWeather = {
+  cloud: number,
+  condition: Condition,
+  temp_f: number,
+  wind_mph: number
+}
+
 function App() {
   const [temp, setTemp] = useState(0);
+  const [location, setLocation] = useState("");
+  const [currentCondition, setCurrentCondition] = useState<Condition>({
+    code: 0,
+    icon: "",
+    text: ""
+  })
+
+  const fetchData = async () => {
+    const result = await fetch(url);
+    result.json().then((json) => {
+      console.log('json:', json);
+      setLocation(json.location.name);
+      setTemp(json.current.temp_f);
+      setCurrentCondition(json.current.condition);
+    })
+  }
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch(url);
-      result.json().then((json) => {
-        console.log('json:', json.current.temp_f);
-        setTemp(json.current.temp_f);
-      })
-    }
     fetchData();
   }, [])
 
   return (
     <>
+      <div>{location}--</div>
       <div>{temp}</div>
+      <div>{currentCondition.text}</div>
       <WeatherDisplay />
     </>
   );
