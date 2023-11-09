a63676419e8baa321206af7390a2e2ffaa0c607c
aokorodu <aokorodu@gmail.com>
2023-11-09 16:53:53 -0500

    Temp component

---
+++ src/components/temp/index.tsx
@@ -0,0 +1,19 @@
+import styles from "./Temp.module.scss";
+import { TCurrentWeather } from "../../interfaces";
+type TempProps = {
+    max: number,
+    min: number,
+    current: TCurrentWeather
+}
+const Temp = ({ max, min, current }: TempProps) => {
+    return (
+        <><div className={styles.tempHolder}>
+            <div className={styles.temp}>{Math.round(current.temp_f)}</div>
+            <div className={styles.feelsLike}>FEELS LIKE: <strong>{Math.round(current.feelslike_f)}&deg;</strong></div>
+            <div>{`HI:${max} LO:${min}`}</div>
+            <div className={styles.conditions}>{current.condition.text.toUpperCase()}</div>
+        </div></>
+    )
+}
+
+export default Temp;
--- src/components/weather-display/index.tsx
+++ src/components/weather-display/index.tsx
@@ -1,27 +1,17 @@
-import React from "react";
-import { TCurrentWeather, TWeather, TForcastWeather, TForcastDay } from "../../interfaces";
+import { TWeather, TForcastWeather } from "../../interfaces";
 import styles from './WeatherDisplay.module.scss';
-import { WindGraphic } from "../graphics";
 import Wind from "../wind";
+import Temp from "../temp";
 
 const WeatherDisplay = ({ current, forecast }: TWeather) => {
     const f: TForcastWeather = forecast;
-    // const arr: TForcastDay[] = f.forcast;
     const max = Math.round(f.forecastday[0].day.maxtemp_f);
     const min = Math.round(f.forecastday[0].day.mintemp_f);
     console.log('f:', f.forecastday[0].day.maxtemp_f)
     return (<>
         <div className={styles.dataHolder}>
-            <div className={styles.tempHolder}>
-                <div className={styles.temp}>{Math.round(current.temp_f)}</div>
-                <div className={styles.feelsLike}>FEELS LIKE: <strong>{Math.round(current.feelslike_f)}&deg;</strong></div>
-                <div>{`HI:${max} LO:${min}`}</div>
-                <div className={styles.conditions}>{current.condition.text.toUpperCase()}</div>
-            </div>
-
-            <div className={styles.windHolder}>
-                <Wind {...current} />
-            </div>
+            <Temp max={max} min={min} current={current} />
+            <Wind {...current} />
         </div>
 
     </>)
