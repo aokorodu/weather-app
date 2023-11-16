80eb4501b2043345571c2d34e9975f52cf448200
aokorodu <aokorodu@gmail.com>
2023-11-16 16:48:20 -0500

    more stuff

--- src/components/wind/index.tsx
+++ src/components/wind/index.tsx
@@ -6,9 +6,9 @@ const Wind = ({ cloud, wind_mph, wind_dir, wind_degree }: TCurrentWeather) => {
     return (
         <>
             <div className={styles.container}>
-                <div>cloud cover: <strong>{cloud}%</strong></div>
-                <div>wind: <strong>{wind_mph}mph {wind_dir}</strong></div>
-                <div>wind degree: <strong>{wind_degree}&deg;</strong></div>
+                <div>CLOUD COVER: <strong>{cloud}%</strong></div>
+                <div>WIND: <strong>{wind_mph}mph {wind_dir}</strong></div>
+                {/* <div>WIND: <strong>{wind_degree}&deg;</strong></div> */}
             </div>
         </>)
 }
